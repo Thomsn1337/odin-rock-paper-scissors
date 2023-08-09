0106d7c5525e8d9a29abd4ce8be53c44b6ecfe96
Thomas Steifler <steiflerthomas@gmail.com>
2023-08-09 22:05:28 +0200

    Implement game loop
    The game loops until either player has reached 5 points.

--- src/index.js
+++ src/index.js
@@ -22,43 +22,80 @@ const decideWinner = (computerChoice, playerChoice) => {
   switch (computerChoice) {
     case "rock":
       if (playerChoice === "rock") {
-        return "Tie";
+        return "tie";
       }
       if (playerChoice === "paper") {
-        return "Player wins";
+        return "player";
       }
       if (playerChoice === "scissors") {
-        return "Computer wins";
+        return "computer";
       }
       break;
     case "paper":
       if (playerChoice === "rock") {
-        return "Computer wins";
+        return "computer";
       }
       if (playerChoice === "paper") {
-        return "Tie";
+        return "tie";
       }
       if (playerChoice === "scissors") {
-        return "Player wins";
+        return "player";
       }
       break;
     case "scissors":
       if (playerChoice === "rock") {
-        return "Player wins";
+        return "player";
       }
       if (playerChoice === "paper") {
-        return "Computer wins";
+        return "computer";
       }
       if (playerChoice === "scissors") {
-        return "Computer wins";
+        return "tie";
       }
       break;
   }
   return "";
 };
 
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
+const game = () => {
+  let playerPoints = 0;
+  let computerPoints = 0;
 
-let winner = decideWinner(computerChoice, playerChoice);
-console.log(winner);
+  while (true) {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+
+    let winner = decideWinner(computerChoice, playerChoice);
+    if (winner === "player") {
+      playerPoints++;
+      alert(
+        `Player won!\nPlayer's points: ${playerPoints}\nComputer's points: ${computerPoints}`,
+      );
+      console.log(computerChoice);
+    }
+    if (winner === "computer") {
+      computerPoints++;
+      alert(
+        `Computer won!\nPlayer's points: ${playerPoints}\nComputer's points: ${computerPoints}`,
+      );
+      console.log(computerChoice);
+    }
+    if (winner === "tie") {
+      alert(
+        `It's a tie!\nPlayer's points: ${playerPoints}\nComputer's points: ${computerPoints}`,
+      );
+      console.log(computerChoice);
+    }
+
+    if (playerPoints === 5) {
+      alert("You won the game!");
+      break;
+    }
+    if (computerPoints === 5) {
+      alert("The computer won the game!");
+      break;
+    }
+  }
+};
+
+game();
