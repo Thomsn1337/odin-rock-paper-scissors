bbd38fbbb3aa3069540d31a0cedec95d6b3392c5
Thomas Steifler <steiflerthomas@gmail.com>
2023-08-14 23:55:46 +0200

    Refactor winner declaration and implement score panel

--- src/index.js
+++ src/index.js
@@ -1,64 +1,58 @@
 const CHOICES = ["rock", "paper", "scissors"];
+let playerPoints = 0;
+let computerPoints = 0;
+
+const scorePanel = document.querySelector(".score");
+const playerScoreText = document.querySelector("#player-score");
+const computerScoreText = document.querySelector("#computer-score");
 
 function getComputerChoice() {
   const randomNum = Math.floor(Math.random() * CHOICES.length);
   return CHOICES[randomNum];
-};
+}
 
 function getPlayerChoice(button) {
   return button.id;
 }
 
-function playGame() {
-  const playerChoice = getPlayerChoice(this);
-  const computerChoice = getComputerChoice();
-
-  console.log(playerChoice);
-  console.log(computerChoice);
+function declareWinner(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) return "Tie";
+  else if (
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper") ||
+    (playerChoice === "rock" && computerChoice === "scissors")
+  )
+    return "Player";
+  else return "Computer";
 }
 
-// const game = () => {
-//   let playerPoints = 0;
-//   let computerPoints = 0;
+function updateScore(winner) {
+  if (winner === "Player") {
+    playerPoints++;
+    playerScoreText.textContent = playerPoints;
+    return;
+  } else if (winner === "Computer") {
+    computerScoreText.textContent = computerPoints;
+    computerPoints++;
+    return;
+  } else return;
+}
 
-//   while (true) {
-//     let playerChoice = getPlayerChoice();
-//     let computerChoice = getComputerChoice();
+function playGame() {
+  if (scorePanel.classList.contains("hidden")) {
+    scorePanel.classList.remove("hidden");
+  }
 
-//     let winner = decideWinner(computerChoice, playerChoice);
-//     if (winner === "player") {
-//       playerPoints++;
-//       alert(
-//         `Player won!\nPlayer's points: ${playerPoints}\nComputer's points: ${computerPoints}`,
-//       );
-//       console.log(computerChoice);
-//     }
-//     if (winner === "computer") {
-//       computerPoints++;
-//       alert(
-//         `Computer won!\nPlayer's points: ${playerPoints}\nComputer's points: ${computerPoints}`,
-//       );
-//       console.log(computerChoice);
-//     }
-//     if (winner === "tie") {
-//       alert(
-//         `It's a tie!\nPlayer's points: ${playerPoints}\nComputer's points: ${computerPoints}`,
-//       );
-//       console.log(computerChoice);
-//     }
+  const playerChoice = getPlayerChoice(this);
+  const computerChoice = getComputerChoice();
+  const winner = declareWinner(playerChoice, computerChoice);
 
-//     if (playerPoints === 5) {
-//       alert("You won the game!");
-//       break;
-//     }
-//     if (computerPoints === 5) {
-//       alert("The computer won the game!");
-//       break;
-//     }
-//   }
-// };
-
-// game();
+  updateScore(winner);
+  if (playerPoints === 5 || computerPoints === 5) {
+    if (playerPoints > computerPoints) console.log("Player wins");
+    else console.log("Computer wins");
+  }
+}
 
 const buttons = document.querySelectorAll(".selection");
 buttons.forEach((button) => {
